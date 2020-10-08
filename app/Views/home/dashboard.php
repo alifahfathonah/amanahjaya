26cab1251d0376986a5dd3f2f74d36b1a00f8198
mundir muzaini <mundinkcoy@gmail.com>
2020-10-08 14:35:04 +0700

    First Push

---
+++ app/Views/home/dashboard.php
@@ -0,0 +1,16 @@
+<?=$this->extend('layout')?>
+<?=$this->section('content')?>
+
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum <?=$nama?></h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+  <h1 class="h3 mb-4 text-gray-800">Assalamualaikum</h1>
+
+
+<?=$this->endSection('content')?>
